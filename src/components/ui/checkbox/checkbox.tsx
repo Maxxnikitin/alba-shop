bca6aaf6b87a3ca2178b75fecc5429fec1a8c94b
Maxx Nikitin <maxxnikitin90@yandex.ru>
2023-02-15 16:18:35 +0700

    feat: add switch component

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,32 +1,27 @@
 import clsx from 'clsx';
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import styles from './checkbox.module.scss';
 import { ICheckbox } from './types';
 
 import { Paragraph } from '..';
 
-export const Checkbox: FC<ICheckbox> = ({
-  className = '',
-  boxClassName = '',
-  label,
-  id = label,
-  amount,
-  ...rest
-}) => {
-  console.log('rr');
-  return (
-    <div className={clsx(styles.container, boxClassName)} {...rest}>
-      <input className={clsx(styles.input, className)} type='checkbox' id={id} />
-      <label className={styles.label} htmlFor={id}>
-        {label}
-      </label>
-      {amount && (
-        <>
-          <div className={styles.dotted} />
-          <Paragraph className={styles.amount}>{amount}</Paragraph>
-        </>
-      )}
-    </div>
-  );
-};
+export const Checkbox: FC<ICheckbox> = memo(
+  ({ className = '', boxClassName = '', label, id, amount, ...rest }) => {
+    console.log('rr');
+    return (
+      <div className={clsx(styles.container, boxClassName)} {...rest}>
+        <input className={clsx(styles.input, className)} type='checkbox' id={id ?? label} />
+        <label className={styles.label} htmlFor={id ?? label}>
+          {label}
+        </label>
+        {amount && (
+          <>
+            <div className={styles.dotted} />
+            <Paragraph className={styles.amount}>{amount}</Paragraph>
+          </>
+        )}
+      </div>
+    );
+  },
+);
