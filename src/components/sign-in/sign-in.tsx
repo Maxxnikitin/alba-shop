69cc198b55305ae4a772e321a37fea9043818382
Maxx Nikitin <maxxnikitin90@yandex.ru>
2023-03-07 14:23:27 +0700

    fix: remove translater

--- src/components/sign-in/sign-in.tsx
+++ src/components/sign-in/sign-in.tsx
@@ -1,8 +1,6 @@
 import clsx from 'clsx';
 import { FC, useState } from 'react';
 
-import { useTranslation } from 'react-i18next';
-
 import styles from './sign-in.module.scss';
 
 import { ISignInProps } from './types';
@@ -12,8 +10,6 @@ import { ETypes, Tabs } from '../ui';
 
 export const SignIn: FC<ISignInProps> = ({ className = '', ...rest }) => {
   const [activeTab, setActiveTab] = useState<ETypes>(ETypes.phone);
-
-  const { t } = useTranslation();
 
   return (
     <section className={clsx(styles.signin, { [className]: className })} {...rest}>
