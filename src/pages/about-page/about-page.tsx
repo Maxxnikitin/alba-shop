c6abe213a0679bfac21bf3cfb0b98af9892ea557
Maxx Nikitin <maxxnikitin90@yandex.ru>
2023-03-07 13:55:53 +0700

    feat: add google map

--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -43,6 +43,14 @@ export const AboutPage: FC<IAboutPageProps> = memo(({ className = '', ...rest })
           <img className={styles.img} src={img} alt={t('alts.about') || ''} />
         </div>
       </div>
+      <iframe
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2819.8111710142707!2d38.958102615451274!3d45.0287585704135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f04fa5fdab425f%3A0xd09e60dccfdc40f4!2z0YPQuy4g0KfQutCw0LvQvtCy0LAsIDEsINCa0YDQsNGB0L3QvtC00LDRgCwg0JrRgNCw0YHQvdC-0LTQsNGA0YHQutC40Lkg0LrRgNCw0LksIDM1MDAwMA!5e0!3m2!1sru!2sru!4v1678171046142!5m2!1sru!2sru'
+        title='address'
+        width='100%'
+        height='375'
+        style={{ border: 'none', maxWidth: 1366 }}
+        loading='lazy'
+      />
     </section>
   );
 });
